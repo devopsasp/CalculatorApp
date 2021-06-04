63a87397e29deef86b4a2b7945ffe81f0a9d730e
A.Sailesh Padmanabhan <arjun_sailesh@yahoo.com>
2021-06-04 08:12:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int add(int a,int b)
+        {
+            return a + b;
+        }
+        public int subtract(int a,int b)
+        {
+            return a - b;
+        }
+        public int multiply (int a,int b)
+        {
+            return a * b;
+        }
+        public int divide(int a,int b)
+        {
+            return a / b;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
